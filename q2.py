b03349ff2ff3ea49f0e748648b39d6996cf098cf
Aviral  Sharma <69363872+aviral-s@users.noreply.github.com>
2020-11-06 22:03:25 +0530

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,29 +1,33 @@
 from sys import exit
+import sys
+n = len(sys.argv)
+format=""
+if n>1:
+    format = sys.argv[1]
 class Date:  
     def __init__(self, d, m, y):  
         self.d = d             
         self.m = m    
         self.y = y    
     
-monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ] 
+md = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ] 
 
-def countLeapYears(d): 
-      
+def countLeap(d):      
     years = d.y  
     if (d.m <= 2) : 
         years =years - 1 
-    return int(years / 4 - years / 100 + years / 400 ) 
+    return (int(years / 4) - int(years / 100) + int(years / 400 ))
    
-def getDifference(dt1, dt2) :  
+def dateDifference(dt1, dt2) :  
     n1 = dt1.y * 365 + dt1.d  
     for i in range(0, dt1.m - 1) : 
-        n1 = n1 + monthDays[i]  
-    n1 =n1 + countLeapYears(dt1)   
+        n1 = n1 + md[i]  
+    n1 =n1 + countLeap(dt1)   
     n2 = dt2.y * 365 + dt2.d  
     for i in range(0, dt2.m - 1) : 
-        n2 =n2 + monthDays[i]  
-    n2 = n2 + countLeapYears(dt2)  
-    return (n2 - n1)  
+        n2 =n2 + md[i]  
+    n2 = n2 + countLeap(dt2)  
+    return abs(n2 - n1)  
    
   
 # Driver program
@@ -46,28 +50,46 @@ elif line.find('/') !=-1:
     str = line.split(' ')
     str = str[1].split('/')
     #print(str)
-    d1 = int(str[0])
-    m1 = int(str[1])
-    y1 = int(str[2][:4])
-    dt1 = Date(d1, m1, y1 )
+    if format[0]=='m' or format[0]=='M':
+        d1 = int(str[1])
+        m1 = int(str[0])
+        y1 = int(str[2][:4])
+        dt1 = Date(d1, m1, y1 )
+    else:
+        d1 = int(str[0])
+        m1 = int(str[1])
+        y1 = int(str[2][:4])
+        dt1 = Date(d1, m1, y1 )
 
 elif line.find('-') !=-1:
     str = line.split(' ')
     str = str[1].split('-')
     #print(str)
-    d1 = int(str[0])
-    m1 = int(str[1])
-    y1 = int(str[2][:4])
-    dt1 = Date(d1, m1, y1 )
+    if format[0]=='m' or format[0]=='M':
+        d1 = int(str[1])
+        m1 = int(str[0])
+        y1 = int(str[2][:4])
+        dt1 = Date(d1, m1, y1 )
+    else:
+        d1 = int(str[0])
+        m1 = int(str[1])
+        y1 = int(str[2][:4])
+        dt1 = Date(d1, m1, y1 )
 
 elif line.find('.') !=-1:
     str = line.split(' ')
     str = str[1].split('.')
     #print(str)
-    d1 = int(str[0])
-    m1 = int(str[1])
-    y1 = int(str[2][:4])
-    dt1 = Date(d1, m1, y1 )
+    if format[0]=='m' or format[0]=='M':
+        d1 = int(str[1])
+        m1 = int(str[0])
+        y1 = int(str[2][:4])
+        dt1 = Date(d1, m1, y1 )
+    else:
+        d1 = int(str[0])
+        m1 = int(str[1])
+        y1 = int(str[2][:4])
+        dt1 = Date(d1, m1, y1 )
 
 else:
     print("Please enter valid date in the file!")
@@ -91,28 +113,46 @@ elif line.find('/') !=-1:
     str = line.split(' ')
     str = str[1].split('/')
     #print(str)
-    d1 = int(str[0])
-    m1 = int(str[1])
-    y1 = int(str[2][:4])
-    dt2 = Date(d1, m1, y1 )
+    if format[0]=='m' or format[0]=='M':
+        d1 = int(str[1])
+        m1 = int(str[0])
+        y1 = int(str[2][:4])
+        dt2 = Date(d1, m1, y1 )
+    else:
+        d1 = int(str[0])
+        m1 = int(str[1])
+        y1 = int(str[2][:4])
+        dt2 = Date(d1, m1, y1 )
 
 elif line.find('-') !=-1:
     str = line.split(' ')
     str = str[1].split('-')
     #print(str)
-    d1 = int(str[0])
-    m1 = int(str[1])
-    y1 = int(str[2][:4])
-    dt2 = Date(d1, m1, y1 )
+    if format[0]=='m' or format[0]=='M':
+        d1 = int(str[1])
+        m1 = int(str[0])
+        y1 = int(str[2][:4])
+        dt2 = Date(d1, m1, y1 )
+    else:
+        d1 = int(str[0])
+        m1 = int(str[1])
+        y1 = int(str[2][:4])
+        dt2 = Date(d1, m1, y1 )
 
 elif line.find('.') !=-1:
     str = line.split(' ')
     str = str[1].split('.')
     #print(str)
-    d1 = int(str[0])
-    m1 = int(str[1])
-    y1 = int(str[2][:4])
-    dt2 = Date(d1, m1, y1 )
+    if format[0]=='m' or format[0]=='M':
+        d1 = int(str[1])
+        m1 = int(str[0])
+        y1 = int(str[2][:4])
+        dt2 = Date(d1, m1, y1 )
+    else:
+        d1 = int(str[0])
+        m1 = int(str[1])
+        y1 = int(str[2][:4])
+        dt2 = Date(d1, m1, y1 )
     
 else:
     print("Please enter valid date in the file!")
@@ -120,7 +160,7 @@ else:
 
 file1.close() 
 file2 = open("output.txt", "w")
-temp = getDifference(dt1,dt2)
+temp = dateDifference(dt1,dt2)
 temp = f'{temp}'
 #temp = str(temp)
 #print(type(temp))
@@ -129,5 +169,5 @@ file2.write(s)
 file2.close()
 #dt1 = Date(10, 9, 2020 ) 
 #dt2 = Date(11, 9, 2020 ) 
-#print(type(getDifference(dt1, dt2)))
-print("Date difference:", getDifference(dt1, dt2), "days") 
+#print(type(dateDifference(dt1, dt2)))
+print("Date difference:", dateDifference(dt1, dt2), "days") 
