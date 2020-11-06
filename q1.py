310c38d6c30ebca4781e164a59a359308edf33d9
Aviral  Sharma <69363872+aviral-s@users.noreply.github.com>
2020-11-06 22:01:08 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -24,50 +24,70 @@ for item in data:
 	a = a+1
 #print(mylist)
 #for i in mydict:
-	#print(mydict[i][0])
-
-mylist1 = list()
-key1 = input()
-key = key1
-while mydict[key][0] != 'None':
-	#print(mydict[key][0])
-	mylist1.append(mydict[key])
-	key = mydict[key][0]
+	#print(mydict[i])
 
 
-mylist2 = list()
-key2 = input()
-key = key2
-while mydict[key][0] != 'None':
-	#print(mydict[key][0])
-	mylist2.append(mydict[key])
-	key = mydict[key][0]
+keylist = list()
+keylist = input().split()
+n = int(keylist[0])
+keylist.pop(0)
 
-if mydict[key1][0] == 'None' or mydict[key2][0] == 'None':
-	print('No leader found')
 
-len1 = len(mylist1)
-len2 = len(mylist2)
-if len1 > len2:
-	diff = len1 - len2
-	mylist1 = mylist1[diff:]
-elif len2 > len1:
-	diff = len2 - len1
-	mylist2 = mylist2[diff:]
 
-for i in range(0,len(mylist1)):
-	if mylist1[i][0] == mylist2[i][0]:
-		print(mylist1[i][0])
-		p = mydict[mylist1[i][0]][1]
-		f = mydict[key1][1]
-		s = mydict[key2][1]
-		print(mylist1[i][0]+" "+"is "+str(f-p)+" level above "+key1)
-		print(mylist1[i][0]+" "+"is "+str(s-p)+" level above "+key2)
+#key1 = input()
+for i in range(0,len(keylist)):
+	mylist1 = list()
+	key = keylist[i]
+	while mydict[key][0] != 'None':
+		#print(mydict[key][0])
+		mylist1.append(mydict[key])
+		key = mydict[key][0]
+	mylist.append(mylist1)
+
+
+
+flag = 0
+for i in range(0,len(keylist)):
+	if mydict[keylist[i]][0] == 'None':
+		print('No leader found')
+		flag=1
 		break
 
+#if mydict[key1][0] == 'None' or mydict[key2][0] == 'None':
+	#print('No leader found')
 
-#print(mylist1)
-#print(mylist2)
-#print(mydict)
-#print(type(data))
-#print(data)
+if flag!=1:
+
+	lenlist = list()
+	for i in range(0,len(keylist)):
+		l = len(mylist[i])
+		lenlist.append(l)
+
+	minl = min(lenlist)
+	for i in range(0,len(keylist)):
+		diff = lenlist[i]-minl
+		mylist[i]=mylist[i][diff:]
+
+
+
+
+	k=0
+	p=0
+	count = 0
+	for i in range(0,len(mylist[0])):
+		count = 0
+		for j in range (0,len(mylist)-1):
+			if mylist[j][i][0] != mylist[j+1][i][0]:
+				break
+			else:
+				count=count+1
+		if count==len(mylist)-1:
+			print(mylist[0][i][0])
+			k=i
+			p=mydict[mylist[0][i][0]][1]
+			break
+
+
+	for i in range(0,len(mylist)):
+		f = mydict[keylist[i]][1]
+		print(mylist[0][k][0]+" "+"is "+str(f-p)+" level above "+keylist[i])
